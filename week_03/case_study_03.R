68366326a65ca0e5bc53519e3ba2529784d9c998
Sunho Hwang <sunhohwa@buffalo.edu>
2021-09-21 10:54:26 -0400

    case study 3 (2)

--- week_03/case_study_03.R
+++ week_03/case_study_03.R
@@ -13,12 +13,14 @@ str(gapminder)
 #???I can't figure out how to remove Kuwait
 gapminder
 gapminder_filtered <- gapminder %>%
-  filter(!is.na("Kuwait"))
+  filter(country !="Kuwait")
 gapminder_filtered
 ?filter
 str(gapminder_filtered)
 #???how do you check if kuwait was filtered? you can only see the first part
-
+#a tibble changed from 1704*6 to 
+#view(gapminder_filtered): you can see graph without kuwait
+view(gapminder_filtered)
 
 
 
@@ -27,12 +29,20 @@ plot1<-ggplot(data=gapminder_filtered,mapping=aes(lifeExp,gdpPercap))+
   geom_point(mapping=aes(color=continent,size= pop/100000))+ scale_y_continuous(trans = "sqrt")+ 
   facet_wrap(~year,nrow=1)+ 
   theme_bw()+ 
-  labs()
+  labs(x="Life Expectancy", y="GDP per Capita", size="Population(100k)",color="continent")
+
+plot1
+ggsave("plot1.png",plot=plot1,width=15)
 # why is y continuous and why do we put it into squared root?
+#
 #when you do color="blue" is this outside of aes() , what is the rule?
+#
 #plus has to come at end of the line
 #try to change labs more
-?facet_wrap  
+?facet_wrap 
+#why is year nrow=1
+
+
 ?labs
 #????I think I didn't get rid of Kuwait
 
