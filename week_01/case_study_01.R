cfa1a60472f95232d52b2b1391e10b9ca255544e
Sunho Hwang <sunhohwa@buffalo.edu>
2021-10-27 19:58:31 -0400

    R

--- week_01/case_study_01.R
+++ week_01/case_study_01.R
@@ -1,2 +1,21 @@
-data(iris)
 
+#download library
+
+library(tidyverse)
+library(reprex)
+library(sf)
+library(spData)
+data(world)
+
+
+view(world)
+
+#code
+
+ggplot(world,aes(x=gdpPercap, y=continent, color=continent))+
+  geom_density(alpha=0.5,color=F)
+
+#add 3 lines
+#1.added fill=continent,2.got rid of y=continent to make y=density 3.xlab() and ylab()
+
+ggplot(world,aes(gdpPercap, color=continent,fill=continent))+geom_density(alpha=0.5,color=F)+xlab("GDP Per Capita")+ylab("Density")
